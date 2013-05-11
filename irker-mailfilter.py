183bc2a8d43e1b6ce7dee04cd713d5fd38a2dd99
Alexander van Gessel <ai0867@gmail.com>
2013-05-11 03:19:36 +0200

    Add support irker JSON objects piped through email
    The original format was pseudo-JSON targets in the subject,
    followed by the privmsg as the body. This was because of a lack of
    available JSON libraries. Simplejson 2.0.9 works on python2.4 though.
    
    This new format isn't actually used anywhere yet,
    but adding support in irkerhook.py is planned.

--- irker-mailfilter.py
+++ irker-mailfilter.py
@@ -5,20 +5,33 @@ import json
 import socket
 import sys
 
-incoming = email.message_from_file(sys.stdin)
 #hardcode everything for now
-subj = incoming.get("Subject")
-if not subj.startswith("irker: "):
-	sys.exit(1)
-stripped = subj.lstrip("irker: ")
-targets = json.loads(stripped)
+incoming = email.message_from_file(sys.stdin)
+
 if incoming.is_multipart():
-	payload = incoming.get_payload(0).get_payload(decode=True)
+    payload = incoming.get_payload(0).get_payload(decode=True)
 else:
-	payload = incoming.get_payload(decode=True)
+    payload = incoming.get_payload(decode=True)
+
+subj = incoming.get("Subject")
+if subj == "irker json":
+    decoded = json.loads(payload)
+    targets = decoded["to"]
+    message = decoded["privmsg"]
+elif subj.startswith("irker: "):
+    stripped = subj.lstrip("irker: ")
+    targets = json.loads(stripped)
+    message = payload
+else:
+    sys.exit(1)
+
+if not isinstance(targets, list):
+    targets = [targets]
+irkermsg = json.dumps({ "to" : targets, "privmsg" : message})
+
 for target in targets:
-	if target not in ["irc://chat.freenode.net/##projectchannel", "irc://chat.freenode.net/#commits"]:
-		sys.exit(1)
-irkermsg = json.dumps({ "to" : targets, "privmsg" : payload })
+    # This list is what you want to configure
+    if target not in ["irc://chat.freenode.net/##projectchannel", "irc://chat.freenode.net/#commits"]:
+        sys.exit(1)
 socket.socket(socket.AF_INET, socket.SOCK_DGRAM).sendto(irkermsg + "\n", ("localhost", 6659))
 
